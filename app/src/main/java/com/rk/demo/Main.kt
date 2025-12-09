744355d28bac1e1a03cfc980a988ae1af34763e9
RohitKushvaha01 <oldisg131@gmail.com>
2025-12-09 21:17:35 +0530

    added comments

--- app/src/main/java/com/rk/demo/Main.kt
+++ app/src/main/java/com/rk/demo/Main.kt
@@ -2,29 +2,25 @@ package com.rk.demo
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import androidx.annotation.Keep
-import com.rk.activities.main.MainActivity
 import com.rk.extension.Extension
 import com.rk.extension.ExtensionAPI
-import com.rk.utils.toast
 
 @Keep
 class Main : ExtensionAPI() {
 
     override fun onPluginLoaded(extension: Extension) {
-        //called when plugin is loaded
+        //called only once when plugin is loaded
         //note: might get called even before the main activity or any ui context is available
+
+        //the plugin will stay in memory until the app is killed by system or force stopped
     }
 
     override fun onActivityCreated(
         activity: Activity,
         savedInstanceState: Bundle?
     ) {
-        if (activity is MainActivity){
-            toast("hello from demo plugin")
-        }
-
+       //most likely never get called because usually activity get created even before the plugins get loaded
     }
 
     override fun onActivityDestroyed(activity: Activity) {
@@ -36,7 +32,7 @@ class Main : ExtensionAPI() {
     }
 
     override fun onActivityResumed(activity: Activity) {
-        
+
     }
 
     override fun onActivitySaveInstanceState(
